b873e02851be27f32fe64d882c0aca7590cb658d
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-09-21 16:19:27 -0700

    scanner: add dedicated app target

---
+++ scanner/capacitor.config.ts
@@ -0,0 +1,14 @@
+const config = {
+  appId: 'com.geoweedo.scanner',
+  appName: 'GeoWeedo Scanner',
+  webDir: '../mobile-shell',
+  server: {
+    url: 'https://geoweedo.com/scanner/app',
+    cleartext: false,
+    allowNavigation: ['geoweedo.com', '*.geoweedo.com'],
+  },
+  android: { allowMixedContent: false },
+  ios: { contentInset: 'automatic' },
+};
+
+export default config;
